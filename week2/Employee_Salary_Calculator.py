10b436a5529868e2e8641b7c4bc7e7a0bf49932a
Muhsil NR <rockriderskerala@gmail.com>
2026-01-28 15:12:14 +0530

    Employee version2

--- week2/Employee_Salary_Calculator.py
+++ week2/Employee_Salary_Calculator.py
@@ -1,12 +1,40 @@
 class employees:
-    def __init__(self,name,id,age):
+    def __init__(self,name,id,age,hour,salary):
         self.name = name
         self.id = id
         self.age = age
-    
-    def gross_salary(self,salary,other_expenses):
-        returnGrossSalary = salary + other_expenses
+        self.salary = salary
+        self.hour = hour
+        self.hourlyRate = 500
+        self.tax = 15
+    def gross_salary(self):
+        other_expenses = (self.hour * self.hourlyRate)
+        returnGrossSalary = self.salary + other_expenses
         return returnGrossSalary
     
-    def deduction(self,value,other_expenses):
-        self.gross_salary(value,other_expenses)
+    def deduction(self):
+        grossSalary = self.gross_salary()
+        returnDeduction = grossSalary - (grossSalary * (self.tax/100)) 
+        return returnDeduction
+
+    def final_salary(self):
+
+        finalSalary = self.gross_salary() + (self.deduction)
+
+        return finalSalary
+    def employee_profile(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"ID: {self.id}")
+        print(f"Gross Salary: {self.gross_salary()}")
+        print(f"Deduction: {self.deduction()}")
+        print(f"final Salary: {self.final_salary}")
+    
+name = input("Enter the name: ")
+id = int(input("Emplyer id number: "))
+age = int(input("Enter the emplyee age: "))
+hours = int(input("Enter the working hours: "))
+salary = int(input("Enter the your salary: "))
+employe = employees(name,id,age,hours,salary)
+
+employe.employee_profile()
