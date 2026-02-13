893428e185f8451a7d77da76cf434396532acf12
Muhsil NR <rockriderskerala@gmail.com>
2026-02-13 14:29:44 +0530

    student File.V6

--- week3/studentFiles.py
+++ week3/studentFiles.py
@@ -1,25 +1,26 @@
 import csv
 import os
 
+
 class students:
-    def __init__(self,list):
+    def __init__(self, list):
         self.students_list = list
         self.valid = 0
         self.non_valid = 0
         self.validity_count()
 
-    def validity(self,student):
-        if student["mark"].isdigit() and int(student['mark'])<= 100 and int(student['mark']) >= 0:
+    def validity(self, student):
+        if student["mark"].isdigit() and int(student['mark']) <= 100 and int(student['mark']) >= 0:
             return True
         else:
             return False
-        
-    def validity_count(self):        
+
+    def validity_count(self):
         for student in self.students_list:
             if self.validity(student):
                 self.valid += 1
         else:
-            self.non_valid +=1
+            self.non_valid += 1
 
     def print_Students(self):
         for student in self.students_list:
@@ -29,18 +30,14 @@ class students:
                 print(f"Name {student["name"]}\t Marks Invalid\n")
 
     def students_average(self):
-        sum  = 0
+        sum = 0
         for student in self.students_list:
             if self.validity(student):
                 sum += int(student['mark'])
-            
+
         avg = sum/len(self.students_list)
         return avg
 
-    
- 
-
-    
 
 filename = "studentFile.csv"
 student_list = []
@@ -53,31 +50,30 @@ try:
     else:
 
         raise ValueError("The File doesn't exist")
-    
-    with open(filename,mode = 'r')as file:
+
+    with open(filename, mode='r')as file:
 
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
 
             student_list.append(row)
 
-    s = students(student_list)
+    while True:
+        s = students(student_list)
 
-    print("Print Studnets[1]\tPrintValidity[2]\tStudents Average Marks[3]")
-    x = int(input("Instruction: "))
-    if x == 1:
-        s.print_Students()
-    elif x == 2:
-        print(f"Valid: {s.valid}")
-        print(f"Non Valid: {s.non_valid}")
-    elif x == 3:
-        print(f"Average Mark = {s.students_average()}")
-        
+        print("\tPrint Studnets[1]\tPrintValidity[2]\tStudents Average Marks[3]\tQuit[4]\n")
+        x = int(input("Instruction: "))
+        if x < 1 or x > 4:
+            raise ValueError("Input Out of bound")         
+        elif x == 4:
+            break
+        elif x == 1:
+            s.print_Students()
+        elif x == 2:
+            print(f"Valid: {s.valid}")
+            print(f"Non Valid: {s.non_valid}")
+        elif x == 3:
+            print(f"Average Mark = {s.students_average()}")
+
 except ValueError as e:
     print(e)
-
-
-
-
-
-
