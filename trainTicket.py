2e8c9357eb127cb0db51543c2dff1eb7f6bd7da8
Muhsil NR <rockriderskerala@gmail.com>
2026-02-18 15:01:45 +0530

    Mentor Week 3 Version3

--- trainTicket.py
+++ trainTicket.py
@@ -8,23 +8,32 @@ try:
         if x == 2:
             break
         elif x < 1 or x > 2:
-            raise ValueError("Value out of bound❌")
+            raise ValueError("Arithematic Error❌")
         elif x ==1:
             print(f"Available seats:{availableSeats}")
             tickets = int(input(f"Enter number of tickets: "))
             print()
-            if tickets > availableSeats or tickets < 1:
-                raise ValueError("Value out of bound")
+            if tickets < 1:
+                print("Arithematic Error❌")
+                break
+            if tickets > availableSeats:
+                print("Overflow Error")
+                break
             price = int(input("Enter ticket price: "))
             print()
-            if price < 1 or price > 20000: 
-                raise ValueError("Value out of bound❌")
+            if price < 1 : 
+                print("Run time Error❌")
+                break
+            
+            if price > 20000:
+                print("Exception")
+                break
             availableSeats -= tickets
         
             print(f"Booking Succesful☑️")
             print(f"Total Cost: {tickets * price}")
             print(f"Remaining seats: {availableSeats}")       
 except ValueError as e:
-    print("Invalid Input❌")
+    print("ValueError")
         
         
