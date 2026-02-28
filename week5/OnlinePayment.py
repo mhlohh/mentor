f79f94db35a1732544d7ac69cbce4ce2f7b47716
Muhsil NR <rockriderskerala@gmail.com>
2026-02-28 22:29:56 +0530

    week5 v4

--- week5/OnlinePayment.py
+++ week5/OnlinePayment.py
@@ -1,48 +1,60 @@
+
 class Payment:
-    def __init__(self,amount):
+    def __init__(self,amount,method):
         self.amount = amount
+        self.paymet_method = method
         
     def pay(self):
         print("Payment Processing...")
         print(f"amount: {self.amount} sucessful")
-        print("Payment method Normal Payment")
-class CreditCardPayment:
+        print(f"Payment Method: {self.paymet_method}\n")
+
+class NormalMethod(Payment):
+    
     def __init__(self,amount):
-        self.amount = amount
+        self.paymet_method = "Normal Payment"
+        super().__init__(amount,self.paymet_method)
         
-    def pay(self):
-        print("Payment Processing...")
-        print(f"amount: {self.amount} sucessful")
-        print("Payment method Credit Card Payment")
-
-class UPIPayment:
+class CreditCardPayment(Payment):
+    
     def __init__(self,amount):
-        self.amount = amount
-        
-    def pay(self):
-        print("Payment Processing...")
-        print(f"amount: {self.amount} sucessful")
-        print("Payment method UPI payment")
+        self.payment_method = "Credit Card Payment"
+        super().__init__(amount,self.payment_method)
 
-class WalletPayment:
+    
+class UPIPayment(Payment):
     def __init__(self,amount):
-        self.amount = amount
+        self.payment_method = "UPI Payment"
+        super().__init__(amount,self.payment_method)
         
-    def pay(self):
-        print("Payment Processing...")
-        print(f"amount: {self.amount} sucessful")
-        print("Payment method Wallet Payment")
+    
+class WalletPayment(Payment):
+    def __init__(self,amount):
+        self.payment_method = "Wallet Payment"
+        super().__init__(amount,self.payment_method)
+        
+    
 
-amount = 1000
-payment = [
-    Payment(1000),
-    CreditCardPayment(amount),
-    UPIPayment(amount),
-    WalletPayment(amount)
-]
 
-for type in payment:
+try:
+    amount = int(input("\nEnter Amount: "))
+    print("\n\tNormal Payment [1]\tUPI Payment [2]\tCredit Payment [3]\tWallet Payemt [4]\n")
+    
+    x = int(input("Enter the Input: "))
     print()
-    type.pay()
-    print()
-        
+    if x < 1 or x > 4:
+        raise ValueError("Invalid Input❌")
+    elif x == 1:
+        pmt = NormalMethod(amount)
+        pmt.pay()
+    elif x == 2:
+        pmt = UPIPayment(amount)
+        pmt.pay()
+    elif x == 3:
+        pmt = CreditCardPayment(amount)
+        pmt.pay()
+    elif x == 4:
+        pmt = WalletPayment(amount)
+        pmt.pay()
+except ValueError as e:
+    print(f"Value Erro: {e}")
--- week5/vehicle.py
+++ week5/vehicle.py
@@ -2,7 +2,12 @@ class Vehicle:
     def __init__(self,brand,speed,fuel_type):
         self.brand = brand
         self.speed = speed
+        if self.speed < 0:
+            raise ValueError("Speed should be positive")
         self.fuel_type = fuel_type
+        self.fuel_types = ["Petrol","Deisel"]
+        if self.fuel_type not in self.fuel_types:
+            raise ValueError("Fuel type : Petrol or Deisel")
         
     def display_inf(self):
         print(f"Brand: {self.brand}")
@@ -12,6 +17,8 @@ class Vehicle:
 class Car(Vehicle):
     def __init__(self,brand,speed,fuel_type,no_doors):
         self.no_doors = no_doors
+        if self.no_doors < 1:
+            raise ValueError("Doors should be positive Value ❌")
         super().__init__(brand,speed,fuel_type)
     def display_info(self):
         print(f"Brand: {self.brand}")
@@ -25,6 +32,7 @@ class Bike(Vehicle):
     def __init__(self, brand, speed, fuel_type,engine_capacity):
         super().__init__(brand, speed, fuel_type)
         self.engine_capacity = engine_capacity
+        
     
     def display_info(self):
         print(f"Brand: {self.brand}")
@@ -35,8 +43,8 @@ class Bike(Vehicle):
 
 
 
-car = Car("Ford",180,"petrol",4)
-bike = Bike("R15",100,"petrol",200)
+car = Car("Ford",180,"Petrol",4)
+bike = Bike("R15",100,"Petrol",200)
 
 print()
 car.display_info()
